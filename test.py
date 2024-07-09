2cd2ae328a970ac65113c4bca338de7aed660a19
ViceBeak <vicebeak999@gmail.com>
2024-07-09 19:37:15 +0800

    provide recommendations

--- test.py
+++ test.py
@@ -16,6 +16,7 @@ def read_dataset():
         idioms = json.load(_dataset)
     remains_label.config(text=f"剩余词语数量：{len(idioms)}")
     calculate_frequency()
+    calculate_recommendations()
 
 
 def read_dataset_infinite():
@@ -24,10 +25,26 @@ def read_dataset_infinite():
         idioms = json.load(_dataset)
     remains_label.config(text=f"剩余词语数量：{len(idioms)}")
     calculate_frequency()
+    calculate_recommendations()
+
+
+def min_max_normalize(data):
+    second_items = [x[1] for x in data]
+    min_val = min(second_items)
+    max_val = max(second_items)
+    a, b = 0, 1
+
+    def normalize(value):
+        return a + (value - min_val) * (b - a) / (max_val - min_val)
+
+    normalized_data = [(x[0], normalize(x[1])) for x in data]
+
+    return normalized_data
 
 
 def calculate_frequency():
-    global idioms
+    global idioms, character_count_normalized, shengmu_count_normalized, yunmu_count_normalized, \
+        shengdiao_count_normalized
     character_frequency = {}
     shengmu_frequency = {}
     yunmu_frequency = {}
@@ -59,17 +76,74 @@ def calculate_frequency():
                 shengdiao_frequency[shengdiao] += 1 / len(idioms)
 
     character_count = [(key, round(value, 4)) for key, value in character_frequency.items()]
-    character_count.sort(key=lambda x: x[1], reverse=True)
+    character_count = sorted(character_count, key=lambda x: x[1], reverse=True)
+    character_count_normalized = min_max_normalize(character_count)
     frequency_character.config(text=f"{character_count[:10]}")
+
     shengmu_count = [(key, round(value, 4)) for key, value in shengmu_frequency.items()]
-    shengmu_count.sort(key=lambda x: x[1], reverse=True)
+    shengmu_count = sorted(shengmu_count, key=lambda x: x[1], reverse=True)
+    shengmu_count_normalized = min_max_normalize(shengmu_count)
     frequency_shengmu.config(text=f"{shengmu_count[:10]}")
+
     yunmu_count = [(key, round(value, 4)) for key, value in yunmu_frequency.items()]
-    yunmu_count.sort(key=lambda x: x[1], reverse=True)
+    yunmu_count = sorted(yunmu_count, key=lambda x: x[1], reverse=True)
+    yunmu_count_normalized = min_max_normalize(yunmu_count)
     frequency_yunmu.config(text=f"{yunmu_count[:10]}")
+
     shengdiao_count = [(key, round(value, 4)) for key, value in shengdiao_frequency.items()]
-    shengdiao_count.sort(key=lambda x: x[1], reverse=True)
+    shengdiao_count = sorted(shengdiao_count, key=lambda x: x[1], reverse=True)
+    shengdiao_count_normalized = min_max_normalize(shengdiao_count)
     frequency_shengdiao.config(text=f"{shengdiao_count}")
+
+
+def calculate_recommendations():
+    global idioms, all_idioms, character_count_normalized, shengmu_count_normalized, yunmu_count_normalized, \
+        shengdiao_count_normalized, weight_entries
+    conservative_scores = {}
+    aggressive_scores = {}
+    character_score = {x[0]: x[1] for x in character_count_normalized}
+    shengmu_score = {x[0]: x[1] for x in shengmu_count_normalized}
+    yunmu_score = {x[0]: x[1] for x in yunmu_count_normalized}
+    shengdiao_score = {x[0]: x[1] for x in shengdiao_count_normalized}
+
+    for idiom in all_idioms:
+        aggressive_scores[idiom] = conservative_scores[idiom] = 0
+        shown = {}
+        for character in idiom:
+            if character in character_score and character not in shown:
+                aggressive_scores[idiom] += character_score[character] * int(weight_entries[0].get())
+                if idiom in idioms:
+                    conservative_scores[idiom] += character_score[character] * int(weight_entries[0].get())
+                shown[character] = True
+        for shengmu in all_idioms[idiom]["shengmu"]:
+            if not shengmu:
+                shengmu = "无声母"
+            if shengmu in shengmu_score and shengmu not in shown:
+                aggressive_scores[idiom] += shengmu_score[shengmu] * int(weight_entries[1].get())
+                if idiom in idioms:
+                    conservative_scores[idiom] += shengmu_score[shengmu] * int(weight_entries[1].get())
+                shown[shengmu] = True
+        for yunmu in all_idioms[idiom]["yunmu"]:
+            if yunmu in yunmu_score and yunmu not in shown:
+                aggressive_scores[idiom] += yunmu_score[yunmu] * int(weight_entries[2].get())
+                if idiom in idioms:
+                    conservative_scores[idiom] += yunmu_score[yunmu] * int(weight_entries[2].get())
+                shown[yunmu] = True
+        for shengdiao in all_idioms[idiom]["shengdiao"]:
+            if not shengdiao:
+                shengdiao = "无声调"
+            if shengdiao in shengdiao_score and shengdiao not in shown:
+                aggressive_scores[idiom] += shengdiao_score[shengdiao] * int(weight_entries[3].get())
+                if idiom in idioms:
+                    conservative_scores[idiom] += shengdiao_score[shengdiao] * int(weight_entries[3].get())
+                shown[shengdiao] = True
+
+    aggressive_recommendations = [(key, round(value, 3)) for key, value in aggressive_scores.items()]
+    aggressive_recommendations.sort(key=lambda x: x[1], reverse=True)
+    aggressive_label.config(text=f"{aggressive_recommendations[:5]}")
+    conservative_recommendations = [(key, round(value, 3)) for key, value in conservative_scores.items()]
+    conservative_recommendations.sort(key=lambda x: x[1], reverse=True)
+    conservative_label.config(text=f"{conservative_recommendations[:5]}")
 
 
 if __name__ == "__main__":
@@ -81,6 +155,9 @@ if __name__ == "__main__":
     # 默认模式为标准模式
     with open("data/answers_noted.json", "r") as dataset:
         idioms = json.load(dataset)
+
+    with open("data_infinite/answers_infinite_extra_noted.json", "r") as dataset:
+        all_idioms = json.load(dataset)
 
     # 菜单，选择游戏模式
     menubar = tk.Menu(root)
@@ -172,15 +249,21 @@ if __name__ == "__main__":
     frequency_shengdiao = tk.Label(frequency_frame, text="", bg="white", width=118, bd=2, relief="groove")
     frequency_shengdiao.grid(row=4)
 
+    character_count_normalized = shengmu_count_normalized = yunmu_count_normalized = shengdiao_count_normalized = []
+
     calculate_frequency()
 
     # 权重框架
     weight_frame = tk.Frame(second_frame, bd=2, relief="groove")
     weight_frame.grid(row=1, column=2, padx=2, pady=2)
+    weight_entries = []
     tk.Label(weight_frame, text="权重").grid(row=0, sticky="we")
+
     for i in range(4):
         entry = ttk.Entry(weight_frame, width=5, style="Pixel.TEntry")
+        entry.insert(0, str(40 - i * 10))
         entry.grid(row=i + 1)
+        weight_entries.append(entry)
 
     # 第二部分的按钮框架
     second_button_frame = tk.Frame(second_frame)
@@ -199,14 +282,18 @@ if __name__ == "__main__":
     conservative_frame.grid(row=0, column=0, padx=3, pady=3)
 
     tk.Label(conservative_frame, text="保守策略（不违反\n已知条件）").grid(row=0, column=0)
-    tk.Label(conservative_frame, text="", bg="white", width=115, bd=2, relief="groove").grid(row=0, column=1)
+    conservative_label = tk.Label(conservative_frame, text="", bg="white", width=115, bd=2, relief="groove")
+    conservative_label.grid(row=0, column=1)
 
     # 最大熵框架
     aggressive_frame = tk.Frame(third_frame)
     aggressive_frame.grid(row=1, column=0, padx=3, pady=3)
 
     tk.Label(aggressive_frame, text="最大熵（可能违反\n已知条件）").grid(row=0, column=0)
-    tk.Label(aggressive_frame, text="", bg="white", width=115, bd=2, relief="groove").grid(row=0, column=1)
+    aggressive_label = tk.Label(aggressive_frame, text="", bg="white", width=115, bd=2, relief="groove")
+    aggressive_label.grid(row=0, column=1)
+
+    calculate_recommendations()
 
     root.config(menu=menubar)
     # 运行主循环
